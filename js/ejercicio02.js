70a9051c14cb41e2d84fa896ad64ea7a21a82d77
ajillos <116339959+ajillos@users.noreply.github.com>
2022-11-03 12:42:57 +0100

    Update ejercicio02.js

--- js/ejercicio02.js
+++ js/ejercicio02.js
@@ -1,2 +1,24 @@
-function muestraOculta() {
+var txt1=document.getElementById('contenidos_1')
+var txt2=document.getElementById('contenidos_2')
+var txt3=document.getElementById('contenidos_3')
+function ocul1(e){
+    if(txt1.style.visibility==='hidden'){
+        txt1.style.visibility='visible'
+    }else{
+        txt1.style.visibility='hidden'
+    }
+}
+function ocul2(e){
+    if(txt2.style.visibility==='hidden'){
+        txt2.style.visibility='visible'
+    }else{
+        txt2.style.visibility='hidden'
+    }
+}
+function ocul3(e){
+    if(txt3.style.visibility==='hidden'){
+        txt3.style.visibility='visible'
+    }else{
+        txt3.style.visibility='hidden'
+    }
 }
